a202f5e085ab1f0d82ea535e4afcb7e07e657a09
devonB <barangandevon@gmail.com>
2025-02-20 18:58:35 +0800

    first commit

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,6 @@ import tailwindcss from "@tailwindcss/vite";
 import react from "@vitejs/plugin-react";
 
 export default defineConfig({
+  base: "/todo-project/", // Add this line
   plugins: [tailwindcss(), react()],
 });
